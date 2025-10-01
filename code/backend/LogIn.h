6bba4ed3602623c480f7af5b4b87cb112690ce36
Kendall Reid <kendall.reid@wsu.edu>
2025-09-30 19:54:25 -0700

    Foundation for LogIn.h

--- code/backend/LogIn.h
+++ code/backend/LogIn.h
@@ -3,7 +3,23 @@
 #include <iostream>
 #include <string>
 
+using std::string;
 
+struct User{
+    string username;
+    string password;  // Store hashed password
+};
 
+class LogInManager
+{
+public:
+    LogInManager(string& dbPath);
+    bool LogIn(const string& username, const string& password);
+    bool userExists(const string& username);
+    bool getUser(const std::string& username, User& outUser);
+
+private:
+    string databasePath;
+};
 
 #endif
